b0b1df34d70d7dcaaf12a89341f802b6d5f2ad41
Remi Gau <remi_gau@hotmail.com>
2022-03-30 22:16:11 +0200

    switch CPP SPM to dev branch

--- dataladConfig.sh
+++ dataladConfig.sh
@@ -4,22 +4,22 @@
 
 # raw dataset url (either GIN or openneuro)
 # for DEMO use: git@gin.g-node.org:/cpp-lln-lab/CPP_visMotion-raw.git
-URL_RAW=''
+URL_RAW='git@gin.g-node.org:/RemiGau/V5_high-res_pilot-1_raw.git'
 
 # derivatives sibling url
 # for DEMO use: git@gin.g-node.org:/cpp_brewery/CPP_visMotion-derivatives.git
-URL_DER=''
+URL_DER='git@gin.g-node.org:/cpp_brewery/analysis_V5_high-res_pilot_nighres_derivatives.git'
 
 # derivatives/preproc sibling url
 # for DEMO use: git@gin.g-node.org:/cpp_brewery/CPP_visMotion-cpp_spm-preproc.git
-URL_DER_PREPROC=''
+URL_DER_PREPROC='git@gin.g-node.org:/cpp_brewery/analysis_V5_high-res_pilot_nighres.git'
 
 # derivatives/stats sibling url
 # for DEMO use: git@gin.g-node.org:/cpp_brewery/CPP_visMotion-cpp_spm-stats.git
 URL_DER_STATS=''
 
 # set to "true" instead of "false" if you want to use the bleeding edge version of CPP SPM
-USE_CPP_SPM_DEV=false
+USE_CPP_SPM_DEV=true
 
 
 if [ -z "${URL_RAW}" ]; then
