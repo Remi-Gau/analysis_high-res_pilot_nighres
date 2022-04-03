79e69d34b8fbf6459434f320106a73e62d474f16
Remi Gau <remi_gau@hotmail.com>
2022-04-03 08:42:00 +0200

    make layers

---
+++ code/bidsNighres/nighres_layers.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# to be called from "code/bidsNighres"
+
+participant="pilot001 pilot004 pilot005"
+
+root_dataset=${PWD}/../..
+
+input_dataset=${root_dataset}/inputs/raw/
+
+output_location=${root_dataset}/outputs/derivatives/bidsNighres/
+
+filter_file=${root_dataset}/code/bidsNighres/filter_file.json
+
+echo "${input_dataset}"
+
+python ../lib/bidsNighres/run.py \
+    --input-datasets "${input_dataset}" \
+    --output-location "${output_location}" \
+    --analysis-level subject \
+    --participant-label "${participant}" \
+    --action layer \
+    --nb-layers 6 \
+    --bids-filter-file "${filter_file}"
